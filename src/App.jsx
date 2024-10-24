83eb681b34eb281c47bf4acbe0eb7ad012edf748
Pablo Anjos <pablomatheussilvaanjoss@gmail.com>
2024-10-24 12:44:09 -0300

    Section Xbox series x finalizada

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,30 @@
 import './App.css'
 import { Header } from './components/Header/Header'
+import imgXbox from './assets/images/xbox.png'
+import imgControleXbox from './assets/images/controle-xbox.png'
 
 function App() {
   return (
     <>
         <Header />
+
+        {/* SECTION XBOX SERIES X */}
+        <section className='section-xbox-series-x'>
+                  <div className='container-xbox-series-x'>
+                        <h1>Xbox series X</h1>
+                        <h2>PEDIDO ANTECIPADO</h2>
+                        <img src={imgXbox} alt="Imagem Xbox" />
+                  </div>
+        </section>
+
+        {/* SECTION CONTROLE XBOX */}
+        <section className='controle-xbox'>
+                  <div className='container-controle-xbox'>
+                        <h1>Desempenho aperfeiçoado</h1>
+                        <p>O controle sem fio Xbox traz um design elegante, conforto refinado e compartilhamento instantâneo para um favorito comum.</p>
+                        <img src={imgControleXbox} alt="Imagem controle Xbox" />
+                  </div>
+        </section>
     </>
   )
 }
