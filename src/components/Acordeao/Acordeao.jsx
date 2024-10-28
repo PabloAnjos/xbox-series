e5456507b681223620466c617bd9c4758e794f98
Pablo Anjos <pablomatheussilvaanjoss@gmail.com>
2024-10-28 12:42:45 -0300

    Iniciando interatividade das arrows

--- src/components/Acordeao/Acordeao.jsx
+++ src/components/Acordeao/Acordeao.jsx
@@ -1,13 +1,20 @@
 import './Acordeao.scss'
 import imgArrow from '../../assets/images/arrow.svg'
-
+import { useState } from 'react';
 
 export function Acordeao({ pergunta }) {
+
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    const toggleExpand = () => {
+        setIsExpanded(!isExpanded);
+      };
+
     return (
         <>
-            <div className='acordeon-perguntas'>
+            <div className='acordeon-perguntas' style={{width: isExpanded ? "1152px" : "1152px", height: isExpanded ? "163px" : "100px", transition: "width 0.3s, height 0.3s"}}>
                     <h3>{pergunta}</h3>
-                    <img src={imgArrow} alt="Imagem Seta" />
+                    <img src={imgArrow} alt="Imagem Seta" onClick={toggleExpand} style={{width: isExpanded ? "30px" : "30px"}}/>
             </div>
         </>
     )
